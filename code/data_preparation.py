929e69243685af8ac5fc28c1c6902ac9541693a3
pdpau <56670577+pdpau@users.noreply.github.com>
2024-08-20 17:11:25 +0200

    all executable from main_scraper

---
+++ code/data_preparation.py
@@ -0,0 +1,42 @@
+# --- Executable script for data preparation ---
+
+# 0. --- Import libraries ---
+import sys
+import pandas as pd
+
+# 1. --- Import data and create a dataframe ---
+city = sys.argv[1]
+csv_path = f"../data/raw/{city}/hotels in {city.lower()} with emails.csv"
+input_csv = pd.read_csv(csv_path, sep=",")
+
+
+# 2. --- Clean the data ---
+main_df = input_csv.copy()
+main_df = main_df.reset_index(drop=True)
+main_df.index += 1
+
+# 2.1 Change data types
+main_df = main_df.astype(str)
+main_df["n_reviews"] = main_df["n_reviews"].astype(int)
+main_df["price"] = main_df["price"].str.replace("€", "").str.replace("nan", "0").astype(int)
+
+# 2.2 Drop hotels with no email
+no_emails_out = main_df[~main_df["email"].apply(lambda x: x in ["NO_EMAIL", "nan"])]
+
+# 2.3 Drop duplicate hotels
+no_duplicates = no_emails_out.drop_duplicates()
+
+# 2.4 Reorder columns
+df_ordered = no_duplicates.iloc[:,[0,1,6,3,4,5,8,7,2]]
+
+
+# 3. --- Save into csv and json files ---
+df = df_ordered.copy()
+
+filename = "hotels_" + city.lower()
+# 3.1 Save into csv
+output_path_csv = "../data/processed/" + city + "/" + filename + ".csv"
+df.to_csv(output_path_csv, index=False)
+# 3.2 Save into json
+output_path_json = "../data/processed/" + city + "/" + filename + ".json"
+df.to_json(output_path_json, orient="records")
--- code/main_scraper.js
+++ code/main_scraper.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 
+const { exec } = require("child_process");
+
 const { scrapeGoogleMaps } = require("./scrapers/maps_hotels_scraper");
 const { scrapeEmails } = require("./scrapers/email_hotels_scraper");
 
@@ -8,6 +10,11 @@ const { sleep } = require("./scrapers/aux_functions");
 
 async function main() {
     const search = process.argv[2];
+    if (!search) {
+        console.error("Please provide a search query");
+        return;
+    }
+    const city = search.split(" ")[2].charAt(0).toUpperCase() + search.split(" ")[2].slice(1).toLowerCase();
 
     /* --- Scrape all data from each hotel in Google Maps --- */
     await scrapeGoogleMaps(search);
@@ -22,6 +29,21 @@ async function main() {
     } else {
         console.error("No hotels found");
     }
+
+    /* --- Execute the python file to clean the data --- */
+    const py_file = "./data_preparation.py";
+    exec(`python ${py_file} ${city}`, (error, stdout, stderr) => { /* python3 not working */
+        if (error) {
+            console.error(`Error executing the python file: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`Error executing the python file: ${stderr}`);
+            return;
+        }
+        console.log(`Python file executed successfully: ${stdout}`);
+    });
+
 }
 
 main().catch((error) => {
